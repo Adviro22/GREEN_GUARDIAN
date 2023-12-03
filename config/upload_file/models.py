dbef3b391544f4493dccd81f9fbe18336ed857ee
Stefania <rgarciac4@unemi.edu.ec>
2023-12-02 21:35:21 -0500

    creacion del modelo upload_file

--- config/upload_file/models.py
+++ config/upload_file/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Registro (models.Model):
+    fecha_registro = models.DateField(default=datetime, verbose_name="fecha de registro")
+    nom_imagen =  models.CharField(max_length=100, verbose_name="Nombre de la imagen")
+    id_usuario = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Registro"
+        verbose_name_plural = "Registros"
+        db_table = "Registro"
+        ordering = ["id"]
+
+        
+
+
+
+
