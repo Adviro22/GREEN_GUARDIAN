88965dc1e343c08c61a69de8eb3603aced638e3a
adviro22_laptop <ajosueramos@gmail.com>
2024-01-06 10:27:52 -0500

    Proyecto correcciones

--- show_result/views.py
+++ show_result/views.py
@@ -4,9 +4,10 @@ from upload_file.models import Registro
 def Show_Results(request):
     try:
         user_id = request.session['user_id']
+        user_correo = request.session['correo']
         latest_record = Registro.objects.filter(id_usuario=user_id).latest('fecha_registro')
         print(latest_record.nom_imagen)  
-        return render(request, 'resultados.html', {'latest_record': latest_record})
+        return render(request, 'resultados.html', {'latest_record': latest_record, 'correo': user_correo})
     except Registro.DoesNotExist:
         # Manejar la excepción si no hay registros para el usuario
         print("No se encontraron registros para el usuario.")
